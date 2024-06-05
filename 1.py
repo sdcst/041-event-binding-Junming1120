6d0fa70f8e30d4e8cf4ff7e6b6fb7a6a12e9c81b
Junming1120 <159005069+Junming1120@users.noreply.github.com>
2024-06-05 12:30:36 -0700

    1

---
+++ 1.py
@@ -0,0 +1,56 @@
+import tkinter as tk 
+from tkinter import *
+from tkinter import ttk
+import playsound as p
+
+def a(event):
+    print(event)
+    p.playsound("a.mp3")
+ 
+def b(event):
+    print(event)
+    p.playsound("b.mp3")
+
+def c(event):
+    print(event)
+    p.playsound("c.mp3")
+
+def d(event):
+    print(event)
+    p.playsound("d.mp3")
+ 
+def e(event):
+    print(event)
+    p.playsound("e.mp3")
+
+def f(event):
+    print(event)
+    p.playsound("f.mp3")
+
+win = tk.Tk()
+win.title()
+
+l1 = tk.Label(win,text="Press any button to play a sound")
+l2 = tk.Button(win, text="a", width=20)
+l2.bind("<Button>",a)
+l3 = tk.Button(win, text="最佳歌手",width=20)
+l3.bind("<Button>", b)
+l4 = tk.Button(win, text="演员",width=20)
+l4.bind("<Button>", c)
+l5 = tk.Button(win, text="其实",width=20)
+l5.bind("<Button>", d)
+l6 = tk.Button(win, text="那是你离开了北京的生活",width=20)
+l6.bind("<Button>", e)
+l7 = tk.Button(win, text="陪你去流浪",width=20)
+l7.bind("<Button>", f)
+
+l1.grid(row=1, column=2)
+l2.grid(row=2, column=1)
+l3.grid(row=2, column=2)
+l4.grid(row=2, column=3)
+l5.grid(row=3, column=1)
+l6.grid(row=3, column=2)
+l7.grid(row=3, column=3)
+
+
+win.mainloop()
